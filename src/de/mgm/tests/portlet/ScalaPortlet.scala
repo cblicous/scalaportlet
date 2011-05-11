c6d10f2dbee089d0f340822c38238371d54d1beb
cblicious <cbelka@gmail.com>
2011-05-11 10:00:35 +0200

    Initial Kickoff

---
+++ src/de/mgm/tests/portlet/ScalaPortlet.scala
@@ -0,0 +1,30 @@
+package de.mgm.tests.portlet
+
+
+// import javax.portlet.GenericPortlet
+import javax.portlet.{GenericPortlet => JavaPortlet}
+import javax.portlet.RenderRequest
+import javax.portlet.RenderResponse
+
+class ScalaPortlet extends JavaPortlet {
+	
+	override def doView(request: RenderRequest, response: RenderResponse) = {
+
+        response.setTitle("Scala Test Portlet")
+        response.getWriter().write(
+            <div>
+                <form action={response.createActionURL.toString} method="post">
+                  
+                     <label>Einen Wert <input name="wert" value="Hallo Welt"/></label>
+                  
+                    <hr/>
+                    <p>
+                        <button name="save" value="save">Speichern</button>
+                        <button name="cancel" value="cancel">Doch nicht</button>
+                    </p>
+                </form>
+            </div>.toString )
+    }
+	
+	
+}
